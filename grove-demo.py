fc620374d65fab7bda88576bf5c2d06c1aad1505
Ondřej Krsička <ondrej.krsicka@gmail.com>
2025-10-20 10:20:26 +0200

    string values in node id would create fake relocation conflicts. moved it to constructors, such as string("Hello, world!")

--- grove-demo.py
+++ grove-demo.py
@@ -20,48 +20,51 @@ example = {
     ("edge0", "node0",  "<body>", "content", "node1", "linkedList", "active"),
     ("edge1", "node1", "linkedList", "head", "node2", "<a>", "active"),
     ("edge2", "node2", "<a>", "href", "node3", "attribute", "active"),
-    ("edge3", "node3", "attribute", "value", "http://example.com", "string", "active"),
-    ("edge7", "node2", "<a>", "content", "Click here", "string", "active"),
+    ("edge3", "node3", "attribute", "value", "node6", 'string("http://example.com")', "active"),
+    ("edge7", "node2", "<a>", "content", "node7", 'string("Click here")', "active"),
     ("edge4", "node1", "linkedList", "tail", "node4", "linkedList", "active"),
     ("edge5", "node4", "linkedList", "head", "node5", "<p>", "active"),
-    ("edge6", "node5", "<p>", "content", "Hello, world!", "string", "active"),
+    ("edge6", "node5", "<p>", "content", "node8", 'string("Hello, world!")', "active"),
 }
 root = "node0"
+root_type = "<body>"
 
-def render(edges: set, root_node: str) -> str:
-    edges_from_root = [e for e in edges if e[NODE_ID_FROM] == root_node and e[STATUS] == "active"]
+def render(edges: set, root_node_id: str, root_node_type: str) -> str:
+    edges_from_root = [e for e in edges if e[NODE_ID_FROM] == root_node_id and e[STATUS] == "active"]
     if not edges_from_root:
-        print(root_node, end='')
+        if root_node_type.startswith('string("') and root_node_type.endswith('")'):
+            print(root_node_type[8:-2], end='')
+        else:
+            print(root_node_type, end='')
         return
     
     from_type = edges_from_root[0][NODE_TYPE_FROM]
 
     if from_type == "attribute":
         for edge in edges_from_root:
-            _, _, _, position, to_node, to_type, _ = edge
-            if to_type == "string" and position == "value":
-                print(to_node, end='')
+            if edge[NODE_TYPE_TO].startswith("string") and edge[POSITION_FROM] == "value":
+                render(edges, edge[NODE_ID_TO], edge[NODE_TYPE_TO])
 
     if from_type.startswith("<") and from_type.endswith(">"):
         print(from_type[:-1], end="")
         attributes_edges = [e for e in edges_from_root if e[NODE_TYPE_TO] == "attribute"]
         for attr_edge in attributes_edges:
             print(f' {attr_edge[POSITION_FROM]}="', end='')
-            render(edges, attr_edge[NODE_ID_TO])
+            render(edges, attr_edge[NODE_ID_TO], attr_edge[NODE_TYPE_TO])
             print('"', end="")
         print(">", end="")
         content_edges = [e for e in edges_from_root if e[POSITION_FROM] == "content"]
         for content_edge in content_edges:
-            render(edges, content_edge[NODE_ID_TO])
+            render(edges, content_edge[NODE_ID_TO], content_edge[NODE_TYPE_TO])
         print(f"</{from_type[1:-1]}>", end="")
     
     if from_type == "linkedList":
         head_edges = [e for e in edges_from_root if e[POSITION_FROM] == "head"]
         for head_edge in head_edges:
-            render(edges, head_edge[NODE_ID_TO])
+            render(edges, head_edge[NODE_ID_TO], head_edge[NODE_TYPE_TO])
         tail_edges = [e for e in edges_from_root if e[POSITION_FROM] == "tail"]
         for tail_edge in tail_edges:
-            render(edges, tail_edge[NODE_ID_TO])
+            render(edges, tail_edge[NODE_ID_TO], tail_edge[NODE_TYPE_TO])
 
 # prints <body><a href="http://example.com">Click here</a><p>Hello, world!</p></body>
-render(example, root)
+render(example, root, root_type)
